6e8874a8f559c8d3564296310c3dd2492f75f9b2
Andrey Yatsenko <divinkas@gmail.com>
2019-02-08 00:25:35 +0200

    Delete IUpdateNoticePresenter.java

--- app/src/main/java/com/example/notificeuserapp/Presenter/interfaces/IUpdateNoticePresenter.java
+++
@@ -1,7 +0,0 @@
-package com.example.notificeuserapp.Presenter.interfaces;
-
-import com.example.notificeuserapp.Data.Notice;
-
-public interface IUpdateNoticePresenter {
-    void updateNotice(Notice notice);
-}
