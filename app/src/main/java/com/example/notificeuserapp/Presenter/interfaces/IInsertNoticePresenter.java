f8a613f5f8d3cafbcdd989a9d21b8bc220b3ac19
Andrey Yatsenko <divinkas@gmail.com>
2019-02-08 00:25:26 +0200

    Delete IInsertNoticePresenter.java

--- app/src/main/java/com/example/notificeuserapp/Presenter/interfaces/IInsertNoticePresenter.java
+++
@@ -1,7 +0,0 @@
-package com.example.notificeuserapp.Presenter.interfaces;
-
-import com.example.notificeuserapp.Data.Notice;
-
-public interface IInsertNoticePresenter {
-    void insertNotice(Notice notice);
-}
